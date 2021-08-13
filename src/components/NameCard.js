90fd79ccf4c2b74d137cd0bc894e0a3c238e800e
Goutham <dsgr369@gmail.com>
2021-08-13 11:01:39 +0530

    V1

--- src/components/NameCard.js
+++ src/components/NameCard.js
@@ -3,7 +3,7 @@ import './NameCard.css';
 function NameCard({SuggestedName}) {
     const nameCheapUrl ='https://www.namecheap.com/domains/registration/results/?domain=';
     return (
-        <a href={nameCheapUrl+SuggestedName} target="_blank">
+        <a href={nameCheapUrl+SuggestedName} target="_blank" rel="noopener noreferrer">
             <div className="result-name-card">
                 <p className="result-text" key={SuggestedName}>{SuggestedName}</p>
             </div>
