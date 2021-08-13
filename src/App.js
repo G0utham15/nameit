c7d0a22cd187c4059ccaee28aad11a5cae3d5eef
Goutham <dsgr369@gmail.com>
2021-08-13 11:59:19 +0530

    Used Hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Header from './components/header';
 import Results from './components/results';
@@ -6,29 +6,21 @@ import SearchBar from './components/search';
 
 const name=require("@rstacruz/startup-name-generator");
 
-class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      HeaderTitle:"Nameit",
-      HeaderExpanded:true,
-      SuggestedNames:[]
-    };
+function App() {
+  const [HeaderTitle, setHeaderTitle] = useState("Nameit");
+  const [HeaderExpanded, setHeaderExpanded] = useState(true);
+  const [SuggestedNames, setSuggestedNames] = useState([])
+  const HandleInputChange=(input)=>{
+    setHeaderExpanded(!input);
+    setSuggestedNames(input?name(input):[]);
   }
-  HandleInputChange=(input)=>{
-    this.setState({HeaderExpanded:!input, SuggestedNames:input?name(input):[]});
-  }
-  render(){
-    
-    return (
-      <div>
-        <Header title={this.state.HeaderTitle} HeaderExpanded={this.state.HeaderExpanded}/>
-        <SearchBar HandleInputChange={this.HandleInputChange}/>
-        <Results SuggestedNames={this.state.SuggestedNames}/>
-      </div>
-    );
-  }
-  
+  return (
+    <div>
+      <Header title={HeaderTitle} HeaderExpanded={HeaderExpanded}/>
+      <SearchBar HandleInputChange={HandleInputChange}/>
+      <Results SuggestedNames={SuggestedNames}/>
+    </div>
+  );
 }
 
-export default App;
+export default App
